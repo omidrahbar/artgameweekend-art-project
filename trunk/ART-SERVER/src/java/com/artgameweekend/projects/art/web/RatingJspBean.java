59a0060cda62cf8daea98cc6a776a63bf802cc11
pierre@androidsoft.org <pierre@androidsoft.org@c2dd0ccd-b6f9-2fe6-36c0-c3bc145f12f7>
2010-07-18 22:30:43 +0000

    flag as inappropriate

--- trunk/ART-SERVER/src/java/com/artgameweekend/projects/art/web/RatingJspBean.java
+++ trunk/ART-SERVER/src/java/com/artgameweekend/projects/art/web/RatingJspBean.java
@@ -8,6 +8,8 @@ package com.artgameweekend.projects.art.web;
 import com.artgameweekend.projects.art.business.Tag;
 import com.artgameweekend.projects.art.business.TagDAO;
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -16,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class RatingJspBean implements Serializable
 {
+    private static Logger logger = Logger.getLogger(RatingJspBean.class.getName());
 
     public String doRating( HttpServletRequest request )
     {
@@ -40,6 +43,29 @@ public class RatingJspBean implements Serializable
         catch( Exception e )
         {
             msg = "Sorry, an error occured. Your rating hasn't been correctly saved !";
+            logger.log(Level.SEVERE, "Error saving rating", e);
+        }
+        return msg;
+   }
+
+    public String doFlag( HttpServletRequest request )
+    {
+        String msg = "Your notification has been successfully registered";
+        try
+        {
+        int flag = Integer.parseInt( request.getParameter("flag"));
+        long id = Long.parseLong( request.getParameter("id"));
+
+        TagDAO dao = new TagDAO();
+        Tag tag = dao.findById( id );
+        tag.setInappropriate(flag);
+        dao.update(tag);
+        }
+        catch( Exception e )
+        {
+            msg = "Sorry, an error occured. Your notification hasn't been correctly saved !";
+            logger.log(Level.SEVERE, "Error saving flag", e);
+
         }
         return msg;
    }
