a8ca35fb1bdd9b97765d51be571e3d60429a9ab4
pierre@androidsoft.org <pierre@androidsoft.org@c2dd0ccd-b6f9-2fe6-36c0-c3bc145f12f7>
2010-10-06 23:02:51 +0000

    Layar webservice - POI in radius fix

--- trunk/ART-SERVER/src/java/org/artags/server/web/layar/LayarServlet.java
+++ trunk/ART-SERVER/src/java/org/artags/server/web/layar/LayarServlet.java
@@ -43,16 +43,18 @@ public class LayarServlet extends HttpServlet
 
         String layerName = request.getParameter("layerName");
         String developerId = request.getParameter("developerId");
-        double latitude = Utils.getDouble( request , "lat" , 48.8);
-        double longitude = Utils.getDouble( request , "lon" , 2.3);
-        int max = Utils.getInt(request, "max", params.getMaxPOIs() ); // not provided by Layar
+        double latitude = Utils.getDouble(request, "lat", 48.8);
+        double longitude = Utils.getDouble(request, "lon", 2.3);
+        int max = Utils.getInt(request, "max", params.getMaxPOIs()); // not provided by Layar
+        String radiusParam = request.getParameter("radius");
+        long radius = (radiusParam != null) ? Long.parseLong(radiusParam) : 1000L;
 
         Writer out = response.getWriter();
-        if( developerId == null || ! developerId.equals(Security.LAYAR_DEVELOPER_ID))
+        if (developerId == null || !developerId.equals(Security.LAYAR_DEVELOPER_ID))
         {
-            response.setStatus( HttpServletResponse.SC_UNAUTHORIZED );
-            response.setContentType( Constants.CONTENT_TYPE_TEXT);
-            out.write( "Unauthorized 401");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType(Constants.CONTENT_TYPE_TEXT);
+            out.write("Unauthorized 401");
             out.close();
             return;
         }
@@ -61,71 +63,85 @@ public class LayarServlet extends HttpServlet
 
         JSONObject layer = new JSONObject();
         JSONArray hotspots = new JSONArray();
-        long lat10e6 = (long) (latitude * 1000000 );
-        long lon10e6 = (long) (longitude * 1000000 );
+        long lat10e6 = (long) (latitude * 1000000);
+        long lon10e6 = (long) (longitude * 1000000);
+        int count = 0;
         for (Tag tag : TagService.getNearestTags(latitude, longitude, max))
         {
-            JSONObject poi = new JSONObject();
-            poi.accumulate("distance", tag.getDistance( lat10e6 , lon10e6 ));
-            poi.accumulate("attribution", "ARTags");
-            poi.accumulate("id", tag.getId());
-            poi.accumulate("title", StringEscapeUtils.escapeJava(tag.getName()));
-            poi.accumulate("imageUrl", Constants.URL_SERVER + "/thumbnail?id=" + tag.getKeyThumbnail().getId() );
-            poi.accumulate("lat", tag.getLat10e6() );
-            poi.accumulate("lon", tag.getLon10e6() );
-            poi.accumulate("line2", "Posted : " + tag.getFormatedDate(request.getLocale()));
-            poi.accumulate("line3", "Rating : " + tag.getRating() );
-            poi.accumulate("line4", "Application platform : Android");
-            poi.accumulate("type", params.getType());
-            poi.accumulate("dimension", params.getDimension());
-            
-            // Actions
-            JSONArray actions = new JSONArray();
-            JSONObject action1 = new JSONObject();
-            action1.accumulate("uri", Constants.URL_SERVER + "/display?id=" + tag.getId());
-            action1.accumulate("label", "View tag");
-            actions.add(action1);
-            JSONObject action2 = new JSONObject();
-            action2.accumulate("uri", Constants.URL_SERVER + "/client/rate.jsp?id=" + tag.getId()+"&id_thumbnail=" + tag.getKeyThumbnail().getId());
-            action2.accumulate("label", "Rate this tag");
-            actions.add(action2);
-            JSONObject action3 = new JSONObject();
-            action3.accumulate("uri", Constants.URL_SERVER + "/client/flag.jsp?id=" + tag.getId()+"&id_thumbnail=" + tag.getKeyThumbnail().getId());
-            action3.accumulate("label", "Flag as inappropriate");
-            actions.add(action3);
-            /*
-            JSONObject action4 = new JSONObject();
-            action4.accumulate("uri", Constants.URL_SERVER + "/client/getdraw.jsp");
-            action4.accumulate("label", "Get ARTags Draw");
-            actions.add(action4);
-             */
+            long distance = tag.getDistance(lat10e6, lon10e6);
+            if (distance < radius)
+            {
+                count++;
+                JSONObject poi = new JSONObject();
+                poi.accumulate("distance", distance);
+                poi.accumulate("attribution", "ARTags");
+                poi.accumulate("id", tag.getId());
+                poi.accumulate("title", StringEscapeUtils.escapeJavaScript(tag.getName()));
+                poi.accumulate("imageUrl", Constants.URL_SERVER + "/thumbnail?id=" + tag.getKeyThumbnail().getId());
+                poi.accumulate("lat", tag.getLat10e6());
+                poi.accumulate("lon", tag.getLon10e6());
+                poi.accumulate("line2", "Posted : " + tag.getFormatedDate(request.getLocale()));
+                poi.accumulate("line3", "Rating : " + tag.getRating());
+                poi.accumulate("line4", "Application platform : Android");
+                poi.accumulate("type", params.getType());
+                poi.accumulate("dimension", params.getDimension());
 
-            poi.accumulate("actions", actions);
+                // Actions
+                JSONArray actions = new JSONArray();
+                JSONObject action1 = new JSONObject();
+                action1.accumulate("uri", Constants.URL_SERVER + "/display?id=" + tag.getId());
+                action1.accumulate("label", "View tag");
+                actions.add(action1);
+                JSONObject action2 = new JSONObject();
+                action2.accumulate("uri", Constants.URL_SERVER + "/client/rate.jsp?id=" + tag.getId() + "&id_thumbnail=" + tag.getKeyThumbnail().getId());
+                action2.accumulate("label", "Rate this tag");
+                actions.add(action2);
+                JSONObject action3 = new JSONObject();
+                action3.accumulate("uri", Constants.URL_SERVER + "/client/flag.jsp?id=" + tag.getId() + "&id_thumbnail=" + tag.getKeyThumbnail().getId());
+                action3.accumulate("label", "Flag as inappropriate");
+                actions.add(action3);
+                /*
+                JSONObject action4 = new JSONObject();
+                action4.accumulate("uri", Constants.URL_SERVER + "/client/getdraw.jsp");
+                action4.accumulate("label", "Get ARTags Draw");
+                actions.add(action4);
+                 */
 
-            // Transform values
-            JSONObject transform = new JSONObject();
-            transform.accumulate("rel", true );
-            transform.accumulate("angle", params.getAngle() );
-            transform.accumulate("scale", 1.0 );
-            poi.accumulate("transform", transform);
+                poi.accumulate("actions", actions);
 
-            // Transform values
-            JSONObject object = new JSONObject();
-            object.accumulate("baseURL", Constants.URL_SERVER + "/thumbnail" );
-            object.accumulate("full", "?id=" + tag.getKeyThumbnail().getId() );
-            object.accumulate("reduced", "?id=" + tag.getKeyThumbnail().getId() );
-            object.accumulate("icon", "?id=" + tag.getKeyThumbnail().getId() );
-            object.accumulate("size", 1);
-            poi.accumulate("object", object);
+                // Transform values
+                JSONObject transform = new JSONObject();
+                transform.accumulate("rel", true);
+                transform.accumulate("angle", params.getAngle());
+                transform.accumulate("scale", 1.0);
+                poi.accumulate("transform", transform);
+
+                // Transform values
+                JSONObject object = new JSONObject();
+                object.accumulate("baseURL", Constants.URL_SERVER + "/thumbnail");
+                object.accumulate("full", "?id=" + tag.getKeyThumbnail().getId());
+                object.accumulate("reduced", "?id=" + tag.getKeyThumbnail().getId());
+                object.accumulate("icon", "?id=" + tag.getKeyThumbnail().getId());
+                object.accumulate("size", 1);
+                poi.accumulate("object", object);
 
 
-            hotspots.add(poi);
+                hotspots.add(poi);
+            }
         }
         layer.accumulate("hotspots", hotspots);
         layer.accumulate("layer", layerName);
-        layer.accumulate("errorString", "ok");
+        if (count > 0)
+        {
+            layer.accumulate("errorString", "OK");
+            layer.accumulate("errorCode", 0);
+
+        } else
+        {
+            layer.accumulate("errorString", "No tag available at this location. Please try to adjust the filter settings.");
+            layer.accumulate("errorCode", 21);
+        }
         layer.accumulate("morePages", false);
-        layer.accumulate("errorCode", 0);
         layer.accumulate("nextPageKey", null);
 
 
